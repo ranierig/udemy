95c9709ced61485c4032b062f281c37c8e289d66
ranierig <ranierig@gmail.com>
2025-03-16 23:37:29 -0300

    auladez

---
+++ AulaDez/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace AulaDez {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double a = 10 / 8;
+            double b = (double)10 / 8;
+            double c = 10.0 / 8;
+            double d = Math.Pow(b, 3.0);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d.ToString(CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter product price:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vect = Console.ReadLine().Split(' ');
+            string lastName = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+            Console.WriteLine(fullName);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
